1b23ffaebabf358a7179b9d8dc5a1c4d533f49eb
Olga Birkovska <aiba.aki9@gmail.com>
2023-03-10 15:10:35 +0200

    ShippingInfo > LocateShipping page

--- src/pages/LocateShipping/LocateShipping.jsx
+++ src/pages/LocateShipping/LocateShipping.jsx
@@ -1,9 +1,11 @@
 import { ShippingSearchForm } from "components/ShippingSearchForm/ShippingSearchForm";
+import { ShippingInfo } from "components/ShippingInfo/ShippingInfo";
 
 const LocateShipping = () => {
     return (
         <>
             <ShippingSearchForm />
+            <ShippingInfo />
         </>
     )
 }
