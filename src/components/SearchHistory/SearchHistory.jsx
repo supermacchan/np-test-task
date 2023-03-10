d0056891b53ee45119b7783de93e30d30ccba334
Olga Birkovska <aiba.aki9@gmail.com>
2023-03-10 15:15:39 +0200

    SearchHistory > LocateShipping page

---
+++ src/components/SearchHistory/SearchHistory.jsx
@@ -0,0 +1,14 @@
+export const SearchHistory = () => {
+    return (
+        <div>
+            <h2>Історія</h2>
+            <ul>
+                {/* a list of queries will be here */}
+                {/* adding a few test ones temporarily, to adjust the styles */}
+                <li>20400048799001</li>
+                <li>20400048799002</li>
+                <li>20400048799003</li>
+            </ul>
+        </div>
+    )
+}
--- src/pages/LocateShipping/LocateShipping.jsx
+++ src/pages/LocateShipping/LocateShipping.jsx
@@ -1,11 +1,13 @@
 import { ShippingSearchForm } from "components/ShippingSearchForm/ShippingSearchForm";
 import { ShippingInfo } from "components/ShippingInfo/ShippingInfo";
+import { SearchHistory } from "components/SearchHistory/SearchHistory";
 
 const LocateShipping = () => {
     return (
         <>
             <ShippingSearchForm />
             <ShippingInfo />
+            <SearchHistory />
         </>
     )
 }
