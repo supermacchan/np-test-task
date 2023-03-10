4a1c0c818e50fe2b91242a46f246a1168079888d
Olga Birkovska <aiba.aki9@gmail.com>
2023-03-10 15:59:31 +0200

    AppBar styles + main color vars

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,10 +1,12 @@
+import css from './AppBar.module.css';
+
 export const AppBar = () => {
     return (
-        <header>
-            <nav>
+        <header className={css.header}>
+            <nav className={css.navigation}>
                 {/* will be replaced with NavLink later when routing is added */}
-                <button type="button">Перевірити ТТН</button>
-                <button type="button">Список відділень</button>
+                <button type="button" className={css.navLink}>Перевірити ТТН</button>
+                <button type="button" className={css.navLink}>Список відділень</button>
             </nav>
         </header>
     )
