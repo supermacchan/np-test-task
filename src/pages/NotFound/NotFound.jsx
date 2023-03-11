adecf5f228a14bca22117c00f75b1d5a7c0cd2db
Olga Birkovska <aiba.aki9@gmail.com>
2023-03-11 17:57:20 +0200

    404 page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,6 +1,12 @@
+import { Link } from 'react-router-dom';
+import css from './NotFound.module.css';
+
 const NotFound = () => {
     return (
-        <h2>Oops! Page not found.</h2>
+        <div className={css.content}>
+            <h2 className={css.title}>Нажаль, такої сторінки немає.</h2>
+            <Link to="/" className={css.link}>Повернутись на Головну</Link>
+        </div>
     )
 }
 
