09a8a9764272067fc99f23b61924faabe05b90d4
Olga Birkovska <aiba.aki9@gmail.com>
2023-03-12 12:13:00 +0200

    responsive AppBar

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,17 +1,42 @@
 import { NavLink } from "react-router-dom";
+import { Desktop, Tablet, Mobile } from "utils/screens";
 import css from './AppBar.module.css';
 
 export const AppBar = () => {
     return (
         <header className={css.header}>
-            <nav className={css.navigation}>
-                <NavLink to="/" className={css.navLink}>
-                    Перевірити ТТН
-                </NavLink>
-                <NavLink to="/offices" className={css.navLink}>
-                    Список відділень
-                </NavLink>
-            </nav>
+            <Desktop>
+                <nav className={css.navigationD}>
+                    <NavLink to="/" className={css.navLinkD}>
+                        Перевірити ТТН
+                    </NavLink>
+                    <NavLink to="/offices" className={css.navLinkD}>
+                        Список відділень
+                    </NavLink>
+                </nav>
+            </Desktop>
+
+            <Tablet>
+                <nav className={css.navigationT}>
+                    <NavLink to="/" className={css.navLinkT}>
+                        Перевірити ТТН
+                    </NavLink>
+                    <NavLink to="/offices" className={css.navLinkT}>
+                        Список відділень
+                    </NavLink>
+                </nav>
+            </Tablet>
+
+            <Mobile>
+                <nav className={css.navigationM}>
+                    <NavLink to="/" className={css.navLinkM}>
+                        Перевірити ТТН
+                    </NavLink>
+                    <NavLink to="/offices" className={css.navLinkM}>
+                        Список відділень
+                    </NavLink>
+                </nav>
+            </Mobile>
         </header>
     )
 }
