19bcf30a35e66bc38f0263d72a665f0fa979e23c
Olga Birkovska <aiba.aki9@gmail.com>
2023-03-12 12:43:25 +0200

    OfficesList (& Item) responsive

--- src/components/OfficesList/OfficesList.jsx
+++ src/components/OfficesList/OfficesList.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { selectOffices } from "redux/selectors";
 import { OfficesListItem } from "components/OfficesListItem/OfficesListItem";
 import { Loader } from 'components/Loader/Loader';
+import { Desktop, Tablet, Mobile } from "utils/screens";
 import { nanoid } from 'nanoid';
 import css from './OfficesList.module.css';
 
@@ -9,23 +10,70 @@ export const OfficesList = () => {
     const { list, isLoading, error } = useSelector(selectOffices);
 
     return (
-        <div className={css.content}>
-            <ul className={css.list}>
-                { isLoading && <Loader />}
+        <>
+            <Desktop>
+                <div className={css.contentD}>
+                    <ul className={css.listD}>
+                        { isLoading && <Loader />}
 
-                { error &&
-                    <h2 className={css.error}>{ error }</h2>
-                }
+                        { error &&
+                            <h2 className={css.errorD}>{ error }</h2>
+                        }
 
-                { list.length > 0 && 
-                    list.map(office => 
-                        <OfficesListItem 
-                            key={nanoid()}
-                            num={office.number}
-                            address={office.address}
-                        />)
-                }
-            </ul>
-        </div>
+                        { list.length > 0 && 
+                            list.map(office => 
+                                <OfficesListItem 
+                                    key={nanoid()}
+                                    num={office.number}
+                                    address={office.address}
+                                />)
+                        }
+                    </ul>
+                </div>
+            </Desktop>
+        
+            <Tablet>
+                <div className={css.contentT}>
+                    <ul className={css.listT}>
+                        { isLoading && <Loader />}
+
+                        { error &&
+                            <h2 className={css.errorT}>{ error }</h2>
+                        }
+
+                        { list.length > 0 && 
+                            list.map(office => 
+                                <OfficesListItem 
+                                    key={nanoid()}
+                                    num={office.number}
+                                    address={office.address}
+                                />)
+                        }
+                    </ul>
+                </div>
+            </Tablet>
+
+            <Mobile>
+                <div className={css.contentM}>
+                    <ul className={css.listM}>
+                        { isLoading && <Loader />}
+
+                        { error &&
+                            <h2 className={css.errorM}>{ error }</h2>
+                        }
+
+                        { list.length > 0 && 
+                            list.map(office => 
+                                <OfficesListItem 
+                                    key={nanoid()}
+                                    num={office.number}
+                                    address={office.address}
+                                />)
+                        }
+                    </ul>
+                </div>
+            </Mobile>
+        </>
+        
     ) 
 }
--- src/components/OfficesListItem/OfficesListItem.jsx
+++ src/components/OfficesListItem/OfficesListItem.jsx
@@ -1,10 +1,23 @@
+import { Desktop, Tablet, Mobile } from "utils/screens";
 import css from './OfficesListItem.module.css';
 
 export const OfficesListItem = ({ num, address }) => {
     return (
         <li className={css.item}>
-            <p className={css.number}>Відділення № {num}</p>
-            <p className={css.address}>Адреса: {address}</p>
+            <Desktop>
+                <p className={css.numberD}>Відділення № {num}</p>
+                <p className={css.addressD}>Адреса: {address}</p>
+            </Desktop>
+            
+            <Tablet>
+                <p className={css.numberT}>Відділення № {num}</p>
+                <p className={css.addressT}>Адреса: {address}</p>
+            </Tablet>
+
+            <Mobile>
+                <p className={css.numberM}>Відділення № {num}</p>
+                <p className={css.addressM}>Адреса: {address}</p>
+            </Mobile>
         </li>
     )
 }
