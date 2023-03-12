a8a90d53df06225029addfe23c5d5abd42eeeddf
Olga Birkovska <aiba.aki9@gmail.com>
2023-03-12 12:59:54 +0200

    LocateShippingPage responsive layout

--- src/pages/LocateShipping/LocateShipping.jsx
+++ src/pages/LocateShipping/LocateShipping.jsx
@@ -1,16 +1,34 @@
 import { ShippingSearchForm } from "components/ShippingSearchForm/ShippingSearchForm";
 import { ShippingInfo } from "components/ShippingInfo/ShippingInfo";
 import { SearchHistory } from "components/SearchHistory/SearchHistory";
+import { Desktop, Tablet, Mobile } from "utils/screens";
 import css from "./LocateShipping.module.css";
 
 const LocateShipping = () => {
     return (
         <section>
             <ShippingSearchForm />
-            <div className={css.content}>
-                <ShippingInfo />
-                <SearchHistory />
-            </div>
+
+            <Desktop>
+                <div className={css.contentD}>
+                    <ShippingInfo />
+                    <SearchHistory />
+                </div>
+            </Desktop>
+
+            <Tablet>
+                <div className={css.contentT}>
+                    <ShippingInfo />
+                    <SearchHistory />
+                </div>
+            </Tablet>
+            
+            <Mobile>
+                <div className={css.contentM}>
+                    <ShippingInfo />
+                    <SearchHistory />
+                </div>
+            </Mobile>
         </section>
     )
 }
